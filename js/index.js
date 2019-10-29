0b885fbc6abf685f85aef7bc0404c42626efaeea
ChuLiying <edward@thenewslens.com>
2019-10-30 02:56:56 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
 
-    var size = 32;
+    var size = 36;
+    var uniformValue = 0.025;
     var text = new Blotter.Text("Products", {
         family: "lato",
         size: size,
@@ -12,31 +13,34 @@ $(document).ready(function () {
         size: size,
         fill: "#666666"
     });
-    setTimeout(function () {
-        var material = new Blotter.LiquidDistortMaterial();
-        Blotter.Text.padding = 100;
-        material.uniforms.uSpeed.value = 0.2;
-        material.uniforms.uVolatility.value = 0.025;
-        material.uniforms.uSeed.value = 0.11;
-        var blotter = new Blotter(material, { texts: text });
-        var scope = blotter.forText(text);
-        var $container = document.getElementById("product-title");
-        scope.appendTo($container);
 
-        var blotter2 = new Blotter(material, { texts: text2 });
-        var scope2 = blotter2.forText(text2);
-        var $container2 = document.getElementById("news-title");
-        scope2.appendTo($container2);
-    }, 100)
+    var material = new Blotter.LiquidDistortMaterial();
+    Blotter.Text.padding = 100;
+    material.uniforms.uSpeed.value = 0.75;
+    material.uniforms.uVolatility.value = uniformValue;
+    material.uniforms.uSeed.value = 1;
+    material.needsUpdate = true;
+    var blotter = new Blotter(material, { texts: text });
+    blotter.needsUpdate = true;
+    var scope = blotter.forText(text);
+    var $container = document.getElementById("product-title");
+    scope.appendTo($container);
+
+    var blotter2 = new Blotter(material, { texts: text2 });
+    var scope2 = blotter2.forText(text2);
+    var $container2 = document.getElementById("news-title");
+    scope2.appendTo($container2);
 
     $(window).on('scroll', firstView);
     var $waveContainer = $('.wave-container');
+    var $header = $('.header');
+    var $kv = $('.kv');
 
-    function firstView(){
+    function firstView() {
         var nonScroll = $('.wave-container').hasClass("show");
-        if (!nonScroll){
-            $(window).unbind('scroll');
-            $('.header').addClass('header-bg');
+        var _position = document.documentElement.scrollTop || document.body.scrollTop;
+        if (!nonScroll && _position<350) {
+            //$(window).unbind('scroll');
             $('.wave-container, .index-about').addClass('show');
 
             var _position = $(window).height();
@@ -46,16 +50,48 @@ $(document).ready(function () {
             }, 800);
         }
 
-        // else{
-        //     bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-        //     $waveContainer.css("transform","translateY("+bodyScrollTop*0.15+"px)");
-        // }
+        // header 
+       
+
+
+        if (_position < 250) {
+            $kv.removeClass('fadeOut');
+            $header.removeClass('header-bg');
+        }
+        else {
+            $('.wave-container, .index-about').addClass('show');
+            $kv.addClass('fadeOut');
+            $header.addClass('header-bg');
+        }
+
+        higherWave();
+        uniformsV();
     }
+    var $wave = $('.waves');
+    function higherWave(){
+        $wave.addClass('higher');
+        setTimeout(function(){
+            $wave.removeClass('higher');
+        },300)
+    }
+
+   function uniformsV(){
+    if (!trigger ){
+        var trigger = true;
+        scope.material.uniforms.uVolatility.value = 0.045;
+    }
+    
+   
+       setTimeout(function(){
+        trigger = false;
+        scope.material.uniforms.uVolatility.value = 0.025;
+    },300)
+   } 
 
     var $elemental = $('.elemental');
     var $circleInfo = $('.circle-inside').find('li');
 
-    $elemental.on('click', function(){
+    $elemental.on('mouseover', function () {
         $elemental.removeClass("active");
         $circleInfo.removeClass("show")
         var _index = $(this).index();
